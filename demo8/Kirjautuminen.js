118b264f7f0445d2bc6d3537cb5cd934c7de8fbf
JereSaario <jere.saario@edu.keuda.fi>
2022-03-07 10:37:40 +0200

    g

--- demo8/Kirjautuminen.js
+++ demo8/Kirjautuminen.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
-function onkoKirjautunut(nimi) {
+function onkoKirjautunut() {
    if (localStorage.getItem("kirjautunut") === "kylla") {
     document.getElementById("tervetulo_teksti").textContent += ` ${localStorage.getItem("nimi")}`;
     document.getElementById("kirjautumis_lomake").style.display = "none";
@@ -11,3 +11,4 @@ function kirjaudu() {
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kylla");
 }
+
