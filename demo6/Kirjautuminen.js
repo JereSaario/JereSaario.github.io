d5f5d9c6f0f225b0b16d1dc3918ac1c12c40dd57
JereSaario <jere.saario@edu.keuda.fi>
2022-03-07 08:55:54 +0200

    eka

---
+++ demo6/Kirjautuminen.js
@@ -0,0 +1,8 @@
+document.addEventListener("DomContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut(){
+let kirjautunut = 'kylla';
+if(kirjautunut === 'kylla'){
+    document.getElementById('tervetulo_teksti').textContent += 'Jere!';
+}
+}
