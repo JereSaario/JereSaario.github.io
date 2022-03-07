649c03c4f6a37b0dfe4168e0d2b6ece9e1b91341
JereSaario <jere.saario@edu.keuda.fi>
2022-03-07 10:51:47 +0200

    f

--- demo8/Kirjautuminen.js
+++ demo8/Kirjautuminen.js
@@ -4,12 +4,18 @@ function onkoKirjautunut() {
    if (localStorage.getItem("kirjautunut") === "kylla") {
     document.getElementById("tervetulo_teksti").textContent += ` ${localStorage.getItem("nimi")}`;
     document.getElementById("kirjautumis_lomake").style.display = "none";
-    }
     document.getElementById("kirjauduUlos").style.display = "block";
+    }
+    
 }
 
 function kirjaudu() {
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kylla");
-}
 
+}
+function kirjauduUlos() {
+    localStorage.clear();
+    document.getElementById("kirjautumis_lomake").style.display = "block";
+    document.getElementById("kirjauduUlos").style.display = "none";
+}
