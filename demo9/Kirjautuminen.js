81bd85400f50404a8a0e9874abeee3e1b51c88bf
JereSaario <jere.saario@edu.keuda.fi>
2022-03-08 09:05:31 +0200

    tsapul

--- demo9/Kirjautuminen.js
+++ demo9/Kirjautuminen.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
 function onkoKirjautunut() {
+    
    if (localStorage.getItem("kirjautunut") === "kylla") {
     document.getElementById("tervetulo_teksti").textContent += ` ${localStorage.getItem("nimi")}`;
     document.getElementById("kirjautumis_lomake").style.display = "none";
     document.getElementById("kirjauduUlos").style.display = "block";
+    document.getElementById("tervetulo_teksti").style.display = "block";
     }
     
 }
@@ -18,4 +20,5 @@ function kirjauduUlos() {
     localStorage.clear();
     document.getElementById("kirjautumis_lomake").style.display = "block";
     document.getElementById("kirjauduUlos").style.display = "none";
+    document.getElementById("tervetulo_teksti").style.display = "none";
 }
